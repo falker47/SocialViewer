8a029c570f29afd72cce3850b038644b2bd9e410
Falker <66997751+falker47@users.noreply.github.com>
2026-09-24 13:09:58 +0200

    test: cover Facebook share/p aliases

--- app/src/test/java/io/github/falker47/socialviewer/provider/facebook/FacebookUrlPolicyTest.kt
+++ app/src/test/java/io/github/falker47/socialviewer/provider/facebook/FacebookUrlPolicyTest.kt
@@ -37,34 +37,58 @@ class FacebookUrlPolicyTest {
         assertFalse(FacebookUrlPolicy.supports("https", "www.facebook.com", "/kevinloveofficial/"))
         assertFalse(FacebookUrlPolicy.supports("https", "www.facebook.com", "/stories/123/"))
         assertFalse(FacebookUrlPolicy.supports("https", "www.facebook.com", "/groups/example/posts/123/"))
-        assertFalse(FacebookUrlPolicy.supports("https", "www.facebook.com", "/share/p/123/"))
+        assertTrue(FacebookUrlPolicy.supports("https", "www.facebook.com", "/share/p/123/"))
         assertFalse(FacebookUrlPolicy.supports("https", "www.facebook.com", "/share/v/123/"))
         assertFalse(FacebookUrlPolicy.supports("https", "fakefacebook.com", "/alice/posts/123/"))
         assertFalse(FacebookUrlPolicy.supports("http", "www.facebook.com", "/alice/posts/123/"))
     }
 
     @Test
-    fun normalizesOnlyReelShareAliasesForSafeRedirectResolution() {
+    fun normalizesPostAndReelShareAliasesForSafeRedirectResolution() {
         assertEquals(
-            "https://www.facebook.com/share/r/1HNwyf2jVo/",
-            FacebookUrlPolicy.shareReelAliasUrl(
+            FacebookContentKind.REEL,
+            FacebookUrlPolicy.shareAlias(
                 "https",
                 "facebook.com",
                 "/share/r/1HNwyf2jVo/",
-            ),
+            )?.expectedKind,
+        )
+        assertEquals(
+            "https://www.facebook.com/share/r/1HNwyf2jVo/",
+            FacebookUrlPolicy.shareAlias(
+                "https",
+                "facebook.com",
+                "/share/r/1HNwyf2jVo/",
+            )?.aliasUrl,
+        )
+        assertEquals(
+            FacebookContentKind.POST,
+            FacebookUrlPolicy.shareAlias(
+                "https",
+                "www.facebook.com",
+                "/share/p/1GMwhxUrGi/",
+            )?.expectedKind,
+        )
+        assertEquals(
+            "https://www.facebook.com/share/p/1GMwhxUrGi/",
+            FacebookUrlPolicy.shareAlias(
+                "https",
+                "www.facebook.com",
+                "/share/p/1GMwhxUrGi/",
+            )?.aliasUrl,
         )
         assertNull(
             FacebookUrlPolicy.canonicalUrl(
                 "https",
                 "www.facebook.com",
-                "/share/r/1HNwyf2jVo/",
+                "/share/p/1GMwhxUrGi/",
             ),
         )
         assertNull(
-            FacebookUrlPolicy.shareReelAliasUrl(
+            FacebookUrlPolicy.shareAlias(
                 "https",
                 "www.facebook.com",
-                "/share/p/1HNwyf2jVo/",
+                "/share/v/1GMwhxUrGi/",
             ),
         )
     }
