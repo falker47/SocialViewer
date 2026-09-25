99d3b457842244522c3ef1d956efb0d0b9dd0df2
Falker <66997751+falker47@users.noreply.github.com>
2026-09-25 19:50:05 +0200

    revert: keep Reddit provider unchanged

--- app/src/test/java/io/github/falker47/socialviewer/provider/reddit/RedditProviderTest.kt
+++ app/src/test/java/io/github/falker47/socialviewer/provider/reddit/RedditProviderTest.kt
@@ -66,7 +66,7 @@ class RedditProviderTest {
     @Test
     fun resolvesReddItShortLinkThroughSameSafeRedirectGate() {
         val http = FakeHttpClient(
-            finalUrl = "https://www.reddit.com/comments/1abc234/",
+            finalUrl = "https://old.reddit.com/r/android/comments/1abc234/example_post/",
         )
 
         val canonical = canonicalRedditUrlFor(
@@ -78,7 +78,7 @@ class RedditProviderTest {
         )
 
         assertEquals(
-            "https://www.reddit.com/comments/1abc234/",
+            "https://www.reddit.com/r/android/comments/1abc234/example_post/",
             canonical,
         )
     }
